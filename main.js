534dae58d736e4325068285d157849833244809f
miguelmorob79 <131287463+miguelmorob79@users.noreply.github.com>
2023-06-22 12:05:08 +0200

    final

---
+++ main.js
@@ -0,0 +1,44 @@
+//diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
+
+// function contarletra (frase,letra) {
+
+//     let contador=0;
+
+//     for (let i=0; i < frase.lenght;i++){
+
+//         if (frase [i]. ()=== letra. ()){
+//             contador++
+//         }
+
+//         return contador;
+//     }
+//     const cantidadveces = (contarletra(frase,letra))
+// }
+
+
+
+//diseña un algoritmo que imprima los numeros impares entre un numero dado por el usuario y los siguientes 50 numeros
+
+
+// function imprimirNumerosImpares(numerousuario) 
+// {
+
+//     let i= 0;
+//     let numerousuario = numeroinicio;
+
+
+
+//     while ( i < 50) {
+
+//         if ( numerousuario % 2 !=== 0 ) 
+
+//         { console.log = ( numerousuario);  i++ }
+//       }
+//     }
+
+//     const numeroinicio = parseInt (prompt (introducirnumero))
+
+//     imprimirNumerosImpares(numerousuario)
+//--------------------------------------------------------------------------------------------------------------------------
+
+   
